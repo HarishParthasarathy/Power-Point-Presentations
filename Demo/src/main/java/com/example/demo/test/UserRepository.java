84d1c83a0cbc8e45f5fbf7aa838c392155ae13e9
HarishParthasarathy <59193847+HarishParthasarathy@users.noreply.github.com>
2020-03-06 16:50:36 +0530

    Deleted Demo

--- Demo/src/main/java/com/example/demo/test/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.test;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.user.User;
-
-public interface UserRepository extends JpaRepository<User,Integer>{
-
-}
